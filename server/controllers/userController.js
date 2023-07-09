68334a1f3054acb3364ecc3b93b97a21701cc78c
vinayak1809 <vinayakrshinde18@gmail.com>
2023-07-09 19:59:27 +0530

    unknown changes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,7 +4,6 @@ const registerUser = async (req, res, next) => {
   const user = new User({ ...req.body });
   user.save();
   return true;
-  //const user = await User.create();
 };
 
 //bcrypt setup then rest of ui
