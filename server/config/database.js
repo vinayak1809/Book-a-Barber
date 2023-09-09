1fb7aaca884762a43800246604d405327261ae53
vinayak1809 <vinayakrshinde18@gmail.com>
2023-09-09 13:17:19 +0530

    new files added

--- server/config/database.js
+++ server/config/database.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const URI = "mongodb://localhost:27017/Book-a-barber";
+//const URI = process.env.MONGODB_URI;
+//console.log("URi", URI);
 
 const connection = mongoose
   .connect(URI, {
