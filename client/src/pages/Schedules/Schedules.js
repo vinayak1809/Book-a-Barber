33cc1c14e22cbd054de96553c51d402774687ff4
vinayak1809 <vinayakrshinde18@gmail.com>
2023-12-27 12:14:54 +0530

    Schedules handling logic

--- client/src/pages/Schedules/Schedules.js
+++ client/src/pages/Schedules/Schedules.js
@@ -1,25 +1,107 @@
 import "./Schedules.css";
 
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import { getBarborAppointments } from "../../features/user/userSlice";
+import { updateBarberAppointment } from "../../features/Appointment/appointmentSlice";
+import payment from "../../utils/payment";
 
 const Schedules = () => {
   const dispatch = useDispatch();
 
-  const { orders } = useSelector((state) => state.user);
+  const { orders, user } = useSelector((state) => state.user);
   const { currentSalon } = useSelector((state) => state.salons);
+
+  const [orderStatus, setOrderStatus] = useState({});
+
+  const handleStatusChange = (orderId, newStatus) => {
+    setOrderStatus((prevStatus) => ({
+      ...prevStatus,
+      [orderId]: newStatus,
+    }));
+  };
 
   useEffect(() => {
     dispatch(getBarborAppointments(currentSalon[0]._id));
   }, []);
 
+  const update = (id) => {
+    dispatch(updateBarberAppointment({ id: id, status: orderStatus[id] }));
+  };
   return (
+    //amount column
     <div>
-      {orders.map((order) => (
-        <p>{order.totalAmount}</p>
-      ))}
+      <h2>Appointments</h2>
+      <div className="table">
+        {user.role === "user" ? (
+          <table>
+            <tr>
+              <td>ID</td>
+              <td>Salon Name</td>
+              <td>Service Name</td>
+              <td>Date & Time</td>
+              <td>Status</td>
+            </tr>
+
+            {orders.map((order) => (
+              <tr>
+                <td>{order._id}</td>
+                <td>{order.services[0].serviceName}</td>
+                <td>9145789245</td>
+                <td>{order.time}</td>
+                {order.status === "accepted" ? (
+                  <button onClick={() => payment(order._id, order.totalAmount)}>
+                    Payment
+                  </button>
+                ) : order.status === "payment-done" ? (
+                  <p>Payment Done</p>
+                ) : (
+                  <p>Requested</p>
+                )}
+              </tr>
+            ))}
+          </table>
+        ) : (
+          <table>
+            <tr>
+              <td>ID</td>
+              <td>Service Name</td>
+              <td>Phone</td>
+              <td>Date & Time</td>
+              <td>Status</td>
+            </tr>
+
+            {orders.map((order) => (
+              <tr>
+                <td>{order._id}</td>
+                <td>{order.services[0].serviceName}</td>
+                <td>9145789245</td>
+                <td>{order.time}</td>
+                <td>
+                  {order.status !== "payment-done" ? (
+                    <select
+                      onChange={(e) =>
+                        handleStatusChange(order._id, e.target.value)
+                      }
+                      value={orderStatus[order._id] || order.status}
+                    >
+                      <option value="requested">Requested</option>
+                      <option value="accepted">Accepted</option>
+                      <option value="rejected">Rejected</option>
+                      <option value="completed">Completed</option>
+                    </select>
+                  ) : (
+                    <p>Payment Done</p>
+                  )}
+                </td>
+
+                <button onClick={() => update(order._id)}>Update</button>
+              </tr>
+            ))}
+          </table>
+        )}
+      </div>
     </div>
   );
 };
