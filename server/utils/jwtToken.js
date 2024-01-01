f7c8b4538ac98db218c9cc387aacc011ad1c85fe
vinayak1809 <vinayakrshinde18@gmail.com>
2024-01-01 11:34:01 +0530

    hiding password

--- server/utils/jwtToken.js
+++ server/utils/jwtToken.js
@@ -1,5 +1,5 @@
-const sendToken = (user, statusCode, res) => {
-  const token = user.getJWTToken();
+const sendToken = async (user, statusCode, res) => {
+  const token = await user.getJWTToken();
 
   const options = {
     expires: new Date(Date.now() + 60 * 24 * 3600000),
@@ -8,10 +8,13 @@ const sendToken = (user, statusCode, res) => {
     sameSite: "none",
   };
 
+  const newObj = { ...user };
+  delete newObj._doc.password;
+
   res
     .status(statusCode)
     .cookie("token", token, options)
-    .json({ success: true, user: user, token: token });
+    .json({ success: true, user: newObj._doc, token: token });
 };
 
 module.exports = sendToken;
